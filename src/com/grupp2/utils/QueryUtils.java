52489a2f1c873f93dbd6aa4810f6a8628c133b88
Patrik638 <patrik.karlsson.pk@gmail.com>
2019-01-22 20:18:29 +0100

    New class for connection to MySQL.

---
+++ src/com/grupp2/utils/QueryUtils.java
@@ -0,0 +1,14 @@
+package com.grupp2.utils;
+
+import diagnosticwebapplication.beans.UserAccount;
+import diagnosticwebapplication.connection.ConnectMySql;
+import diagnosticwebapplication.utils.Connection;
+import diagnosticwebapplication.utils.Exception;
+import diagnosticwebapplication.utils.ResultSet;
+import diagnosticwebapplication.utils.SQLException;
+import diagnosticwebapplication.utils.Statement;
+import diagnosticwebapplication.utils.String;
+
+public class QueryUtils {
+
+}
