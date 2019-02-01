cdcfba8a780ca91895819780b338ac9d9356ec39
Patrik638 <patrik.karlsson.pk@gmail.com>
2019-02-01 15:39:20 +0100

    For test of method "saveNewsInDB"
    The Main class is modified a bit to test this method.

--- src/com/grupp2/Main.java
+++ src/com/grupp2/Main.java
@@ -10,16 +10,19 @@ public class Main {
 
     // Added booleans to enable/disable others code while developing to make it easier. Disable at some point.
     static boolean runNicklasDevCode = true;
+    static ArrayList<NewsItem> newsArrayList;
+    static String sourceName;
+    static String sourceUrl;
 
     public static void main(String[] args) 
     		throws IOException, ClassNotFoundException, SQLException {
     	
-    	QueryUtils dbQuery = new QueryUtils();
+    	//QueryUtils dbQuery = new QueryUtils();
     	System.out.println("main started");
 
-    	dbQuery.connect();
+    	QueryUtils.connect();
     	// Call method to check connection.
-    	boolean connectedToDb = dbQuery.checkIfDbConnected(); 
+    	boolean connectedToDb = QueryUtils.checkIfDbConnected(); 
     	if(connectedToDb) {
     		System.out.println("Connected to DB");
     	} else {
@@ -28,14 +31,26 @@ public class Main {
 			
 		if (runNicklasDevCode) {
 	        // Prerequisite: Download and associate project with jsour.org jar (Can be downloaded at https://jsoup.org/download)
-			GetNews.getNewsFromDN();
-
-			// Example usage of GetNews.getNewsFromDN method. GetNews.getNewsFromGP works the same way.
-			ArrayList<NewsItem> newsArrayList=GetNews.getNewsFromDN();
+			//GetNews.getNewsFromDN();
+			
+			// Get data from DN
+			sourceName = "DN";
+			sourceUrl = "www.dn.se";
+			newsArrayList=GetNews.getNewsFromDN();
+			QueryUtils.saveNewsInDB(newsArrayList, sourceName, sourceUrl);
+			
+			// Get data from GP
+			sourceName = "GP";
+			sourceUrl = "www.gp.se";
+			newsArrayList=GetNews.getNewsFromGP();
+			QueryUtils.saveNewsInDB(newsArrayList, sourceName, sourceUrl);
+			
+			/*
 			for(NewsItem newsItem:newsArrayList){
 				System.out.println(newsItem.getNewsLink());
 				System.out.println(newsItem.getNewsText());
 			}
+			*/
 		}
     }
 }
--- src/com/grupp2/NewsItem.java
+++ src/com/grupp2/NewsItem.java
@@ -1,7 +1,5 @@
 package com.grupp2;
 
-import java.util.Date;
-
 public class NewsItem {
 
     private String newsText;
